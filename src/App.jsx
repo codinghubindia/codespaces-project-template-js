ec19f27eb4c743f53f541d57969e5e890985472a
Manjunatha N <152291295+codinghubindia@users.noreply.github.com>
2024-09-28 06:50:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Maxx",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "maxx@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
+  linkedIn: "maxx",
+  medium: "English",
   twitter: "microsoft",
   youTube: "Code",
 };
